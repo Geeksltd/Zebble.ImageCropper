ea2c9fda45e3d1bed763e0c0f7dc62264abfb01c
Siavash Mohseni <Siavash.Mohseni@live.com>
2019-07-29 12:21:51 +0100

    Update packages

---
+++ UWP/ImageCropper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Zebble
+{
+    public static partial class ImageCropper
+    {
+        static Task DoShow()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
